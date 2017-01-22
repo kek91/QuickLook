8e721fdf8f9e35a3bfdbccd8d5d6ceac2a8f3ade
kek <kek@teknix.no>
2017-01-22 01:59:51 +0100

    Add support for Windows using Seer
    Requires additional client setup as described in readme

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ Press `Shift+Space` in fman to preview the file currently under the cursor.
 
 ## Installation
 Download [master.zip](https://github.com/mherrmann/QuickLook/archive/master.zip) and extract it to `~/Library/Application Support/fman/Plugins` (so you have `.../Plugins/QuickLook-master`). Restart fman.
+
+**Pre-requisition for Windows:**
+
+- Install [Seer](https://sourceforge.net/projects/ccseer/)
+- Append Seer to your PATH environment variable: `setx path "%path%;C:\Program Files (x86)\Seer\"`
--- quicklook/__init__.py
+++ quicklook/__init__.py
@@ -1,8 +1,14 @@
 from subprocess import Popen
-from fman import DirectoryPaneCommand
+from fman import DirectoryPaneCommand, show_alert
+import platform
 
 class QuickLook(DirectoryPaneCommand):
 	def __call__(self):
 		file_under_cursor = self.pane.get_file_under_cursor()
 		if file_under_cursor:
-			Popen('qlmanage -p "%s"' % file_under_cursor, shell=True)
+			if platform.system() == "Windows":
+				Popen('"seer" "%s"' % file_under_cursor, shell=True)
+			elif platform.system() == "Linux":
+				show_alert("Not implemented")
+			elif platform.system() == "Darwin":
+				Popen('qlmanage -p "%s"' % file_under_cursor, shell=True)
